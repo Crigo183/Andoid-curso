878f30b7b03ea4fa7d81c807b71842b45552fd52
Cristiano <Cristiano@CRISTIANO-PC>
2013-09-28 11:33:51 -0300

    commit das classe tudo

---
+++ Aula2/src/net/warmling/aula2/RssItem.java
@@ -0,0 +1,57 @@
+package net.warmling.aula2;
+
+public class RssItem {
+	
+	private String title;
+	
+	private String description;
+	
+	private String date;
+	
+	private String imageUrl;
+	
+	private String link;
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String iageUrl) {
+		this.imageUrl = iageUrl;
+	}
+	
+	
+
+}
